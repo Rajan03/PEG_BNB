444587797b1ff9bc7f526ac664129dfc7005aa78
Rajan <65783031+Rajan03@users.noreply.github.com>
2023-03-07 23:19:54 +0530

    about page made responsive

--- src/components/headers/SectionHeader.tsx
+++ src/components/headers/SectionHeader.tsx
@@ -1,28 +1,33 @@
 import { HEADER_UNDERLINE } from "@/constant/images";
 import Image from "next/image";
-import {EB_Garamond} from "@next/font/google";
+import { EB_Garamond } from "@next/font/google";
 
 const Font = EB_Garamond({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 type Props = {
   title: string;
   subtitle: string;
 
-    className?: string;
+  className?: string;
 };
 
 const SectionHeader = (props: Props) => {
   return (
-    <div className={"flex flex-col justify-center items-center gap-y-3 " + props.className}>
-      <p className="uppercase text-primary-600 text-lg tracking-wider">
+    <div
+      className={
+        "flex flex-col justify-center items-center gap-y-3 " + props.className
+      }
+    >
+      <p className="uppercase text-primary-600 text-lg tracking-wider text-center">
         {props.subtitle}
       </p>
       <h2
         className={
-          "font-bold text-primary-600 text-5xl tracking-widest " + Font.className
+          "font-bold text-primary-600 text-5xl text-center tracking-widest " +
+          Font.className
         }
       >
         {props.title}
