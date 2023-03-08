5a510c98cddaa48d7a986080f50e5481f5e6beae
Rajan <65783031+Rajan03@users.noreply.github.com>
2023-03-08 22:58:39 +0530

    replaced react datepicker component with other npm

--- src/components/inputs/DatePicker.tsx
+++ src/components/inputs/DatePicker.tsx
@@ -1,42 +1,41 @@
 "use client";
 
 import React, { useState } from "react";
-import { DateRangePicker, RangeKeyDict } from "react-date-range";
+import { DateRange, RangeKeyDict } from "react-date-range";
 
 export default function DateInput(): JSX.Element {
-  const [selectedDate, setSelectedDate] = useState<{
-    startDate: Date;
-    endDate: Date;
-    key: string;
-  }>({
-    startDate: new Date(),
-    endDate: new Date(),
-    key: "selection",
-  });
+  const [selectedDate, setSelectedDate] = useState<
+    {
+      startDate: Date;
+      endDate: Date;
+      key: string;
+    }[]
+  >([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
 
   const onChange = (dates: RangeKeyDict) => {
-    if (dates === null) {
-      setSelectedDate({
-        startDate: new Date(),
-        endDate: new Date(),
-        key: "selection",
-      });
-      return;
-    }
     const { selection } = dates;
-    setSelectedDate({
-      startDate: selection.startDate || new Date(),
-      endDate: selection.endDate || new Date(),
-      key: "selection",
-    });
+    setSelectedDate([
+      {
+        startDate: selection.startDate || new Date(),
+        endDate: selection.endDate || new Date(),
+        key: "selection",
+      },
+    ]);
   };
 
   return (
     <>
-      <DateRangePicker
-        ranges={[selectedDate]}
+      <DateRange
+        editableDateInputs={true}
         onChange={onChange}
-        direction="horizontal"
+        moveRangeOnFirstSelection={false}
+        ranges={selectedDate}
         color={"#e2a69a"}
       />
     </>
